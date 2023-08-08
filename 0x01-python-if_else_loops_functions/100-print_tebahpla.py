453462fbeb87599cb4d8d8ccd8fd89440ccdb1d1
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-08 21:55:33 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 CAP = 2
+
 for i in range(-122, -96):
+    temp = i
     if (CAP % 2) == 0:
-        i *= -1
+        temp *= -1
         CAP += 1
     else:
-        i *= -1
-        i -= 32
+        temp *= -1
+        temp -= 32
         CAP += 1
-    print("{}".format(chr(i)), end="")
+    print("{}".format(chr(i)), end='')
