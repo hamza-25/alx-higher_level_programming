b3e9a510b2032e7a6f8d25ec3c585a28c6ae07f2
hamza-25 <me677499@gmail.com>
2023-08-08 20:58:27 +0000

    add +x to file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 CAP = 2
-
 for i in range(-122, -96):
-    temp = i
     if (CAP % 2) == 0:
-        temp *= -1
+        i *= -1
         CAP += 1
     else:
-        temp *= -1
-        temp -= 32
+        i *= -1
+        i -= 32
         CAP += 1
-    print("{}".format(chr(temp)), end='')
+    print("{}".format(chr(i)), end='')
