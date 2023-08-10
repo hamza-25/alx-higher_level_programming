2a17e502d06adb5d3f3c483f69beca4b124aa0a5
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-10 16:50:40 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{0} + {1} = {2}".format(a, b, add.add(a, b)))
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum)
