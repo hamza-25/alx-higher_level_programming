b739bc70d19d6213295cde2f04e2ece6b7f959e9
hamza-25 <me677499@gmail.com>
2023-08-10 17:44:17 +0000

    func that print name defined by module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
-# uncompyle6 version 3.9.0
-# Python bytecode version base 3.8.0 (3413)
-# Decompiled from: Python 3.8.10 (default, May 26 2023, 14:05:08)
-# [GCC 9.4.0]
-# Embedded file name: hidden_4.py
-# Compiled at: 2021-07-16 17:16:33
-# Size of source mod 2**32: 146 bytes
-
-
-def __init__():
-    return True
-
-
-def my_secret_santa():
-    return True
-
-
-def print_school():
-    return True
-
-
-def print_hidden():
-    return True
-# okay decompiling hidden_4.pyc
+import hidden_4
+if __name__ == "__main__":
+    length = len(dir(hidden_4))
+    list_1 = dir(hidden_4)
+    for i in range(length):
+        if list_1[i][0] != '_':
+            print("{}".format(list_1[i]))
