9304c0b6a526e3486e8e36bb2397fc1c20104251
hamza-25 <me677499@gmail.com>
2023-08-10 15:22:40 +0000

    add to modules 'as'

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)))
+    print("{0} + {1} = {2}".format(a, b, add.add(a, b)))
