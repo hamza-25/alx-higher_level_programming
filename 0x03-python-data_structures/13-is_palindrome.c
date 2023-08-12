93e572959274b985bc07e1c3c7522c2127148b4d
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-12 21:33:56 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,47 @@
 #include "lists.h"
 
 /**
+ * create_node - create node
+ * @number: value int to add
+ * Return: a struct node
+*/
+
+listint_t *create_node(int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * duplicate_list - duplicate list
+ * @head: head of original list
+ * Return: a duplicate list from original
+*/
+
+listint_t *duplicate_list(listint_t *head)
+{
+	listint_t *dup, *c_head, *first_node;
+
+	if (!head)
+		return (NULL);
+	first_node = create_node(head->n);
+	dup = first_node;
+	c_head = head->next;
+	while (c_head)
+	{
+		dup->next = create_node(c_head->n);
+		dup = dup->next;
+		c_head = c_head->next;
+	}
+	return (first_node);
+}
+
+/**
  * is_palindrome - func that check if palindrome
  * @head: head of linked list
  * Return: 1 if palindrome 0 if not
