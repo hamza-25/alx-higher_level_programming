685f223ce7322795d3fd58a662510e38d470dbd9
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-12 20:10:43 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -49,11 +49,12 @@ listint_t *duplicate_list(listint_t *head)
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head, *next, *pre = NULL, *dup = NULL;
+	listint_t *current = *head, *next, *pre = NULL, *dup = NULL, *f;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	dup = duplicate_list(*head);
+	 f = dup;
 	if (!dup)
 		return (1);
 	while (current)
@@ -69,10 +70,12 @@ int is_palindrome(listint_t **head)
 	{
 		if (current->n != dup->n)
 		{
+			free_listint(f);
 			return (0);
 		}
 		current = current->next;
 		dup = dup->next;
 	}
+	free_listint(f);
 	return (1);
 }
