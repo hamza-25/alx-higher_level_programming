096fe7a985f54f1f1a8b2f7b8806becbcb1d3ed2
hamza-25 <me677499@gmail.com>
2023-08-12 18:38:45 +0000

    add two function create and dup

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,47 @@
 #include "lists.h"
 
 /**
+ * create_node - create node
+ * @number: value int to add
+ * Return: a struct node
+*/
+
+listint_t *create_node(int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * duplicate_list - duplicate list
+ * @head: head of original list
+ * Return: a duplicate list from original
+*/
+
+listint_t *duplicate_list(listint_t *head)
+{
+	listint_t *dup, *c_head, *first_node;
+
+	if (!head)
+		return (NULL);
+	first_node = create_node(head->n);
+	dup = first_node;
+	c_head = head->next;
+	while (c_head)
+	{
+		dup->next = create_node(c_head->n);
+		dup = dup->next;
+		c_head = c_head->next;
+	}
+	return (first_node);
+}
+
+/**
  * is_palindrome - func that check if palindrome
  * @head: head of linked list
  * Return: 1 if palindrome 0 if not
@@ -8,9 +49,12 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head, *next, *pre = NULL, *rev;
+	listint_t *current = *head, *next, *pre = NULL, *dup = NULL;
 
 	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	dup = duplicate_list(*head);
+	if (!dup)
 		return (1);
 	while (current)
 	{
@@ -19,14 +63,14 @@ int is_palindrome(listint_t **head)
 		pre = current;
 		current = next;
 	}
-	rev = pre;
+	*head = pre;
 	current = *head;
-	while (current)
+	while (dup)
 	{
-		if (current->n != rev->n)
+		if (current->n != dup->n)
 			return (0);
 		current = current->next;
-		rev = rev->next;
+		dup = dup->next;
 	}
 	return (1);
 }
