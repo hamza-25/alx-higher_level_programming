4be337c8f65ba0e8c83a92a6cd169d8cc9cb6d54
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-12 15:04:33 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,6 +10,8 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *current = *head, *next, *pre = NULL, *rev;
 
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 	while (current)
 	{
 		next = current->next;
