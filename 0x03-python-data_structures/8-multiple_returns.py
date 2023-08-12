66ffe3f44471e908c511d0644ec89eca8814bb3f
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-12 13:03:51 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = 0
-    char = ""
-    for i in sentence:
-        length += 1
-    if sentence[0] == " ":
-        char = 'None'
+    tup = ()
+    length = len(sentence)
+    if length == 0:
+        tup = length, "None"
     else:
-        char = sentence[0]
-    tup = (length, char)
-    return (tup)
+        tup = length, sentence[0]
+    return tup
