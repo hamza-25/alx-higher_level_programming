897157f0037e7ac6486a6587e8b7e4f57929389e
hamza-25 <me677499@gmail.com>
2023-08-13 13:52:12 +0000

    fix pycode style and indentetion

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-  from magic_calculation_102 import sub, add
-  if a > b:
-    return sub(a, b)
-  else:
-    sum = add(a, b)
-    for j in range(4, 6):
-      sum = add(sum, j)
-    return sum
-  return 0
+    from magic_calculation_102 import sub, add
+    if a > b:
+        return sub(a, b)
+    else:
+        sum = add(a, b)
+        for j in range(4, 6):
+            sum = add(sum, j)
+        return sum
