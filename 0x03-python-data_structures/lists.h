85327ba06dff9252056e2798dad675a681663b1d
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-13 22:18:14 +0100

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,6 +21,7 @@ struct listint_s *next;
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void print_python_list_info(PyObject *p);
 
 int is_palindrome(listint_t **head);
 listint_t *duplicate_list(listint_t *head);
