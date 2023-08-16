0848d7b03c4b4f390ad2a77b07270a8b5ad2b213
hamza-25 <me677499@gmail.com>
2023-08-16 11:41:33 +0000

    add unique value and sum them

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    total = 0
+    uniq =[i for i in my_list if my_list.count(i)==1]
+    for j in uniq:
+        total += j + 1
+    return total
