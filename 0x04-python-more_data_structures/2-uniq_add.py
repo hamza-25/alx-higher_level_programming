20d7218998b99306cd984519370b422202fa33d1
hamza-25 <me677499@gmail.com>
2023-08-16 12:04:53 +0000

    delete function module and replace with loops

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     total = 0
-    uniq = [i for i in my_list if my_list.count(i) == 1]
-    for j in uniq:
-        total += j + 1
+    list_len = len(my_list) - 1
+    for i in range(len(my_list)):
+        while list_len >= 0:
+            if my_list[i] == my_list[list_len]:
+                break
+            list_len -= 1
+        else:
+            total += my_list[i]
     return total
