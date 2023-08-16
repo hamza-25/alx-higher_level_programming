1e021cdfe50533ca7609451ee0c5d846b17da861
hamza-25 <me677499@gmail.com>
2023-08-16 12:11:23 +0000

    add set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    total = 0
-    list_len = len(my_list) - 1
-    for i in range(len(my_list)):
-        while list_len >= 0:
-            if my_list[i] == my_list[list_len]:
-                break
-            list_len -= 1
-        else:
-            total += my_list[i] + 1
-    return total
+    my_set = set(my_lis)
+    return sum(my_set)
