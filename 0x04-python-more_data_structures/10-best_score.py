cfe4f83cc9dbf7f0506398765ae78791a53977ae
hamza-25 <me677499@gmail.com>
2023-08-16 14:21:22 +0000

    find the biggest score in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    big = 0
+    for key in a_dictionary:
+        if a_dictionary[key] >= big:
+            big = a_dictionary[key]
+    return big
