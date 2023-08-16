f4a8814d8d50a7aa49b537f8c30ff64db07f2d20
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-16 15:33:05 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if Not a_dictionary :
+    if Not a_dictionary:
         return (None)
     big = max(a_dictionary, key=a_dictionary.get)
     return big
