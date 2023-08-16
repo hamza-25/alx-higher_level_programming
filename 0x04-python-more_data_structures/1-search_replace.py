6a302cc8f4e6fc63a4b99ef42c4499df47bb7220
hamza-25 <me677499@gmail.com>
2023-08-16 11:21:43 +0000

    function that replace int in a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) == 0:
+        return my_list
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
