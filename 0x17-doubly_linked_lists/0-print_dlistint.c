37edd50d1b99a53f4fffe77714013e0f2cc186bc
hamza-25 <me677499@gmail.com>
2023-08-17 10:42:25 +0000

    print a dlist and return number of node

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - print each node
+ *
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+	const dlistint_t *current = h;
+
+	while(current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
