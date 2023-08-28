8dc3e53b6e98214ab65b1296910cb3a172992613
hamza-25 <me677499@gmail.com>
2023-08-28 15:06:42 +0000

    add finally to the func

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,14 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     new = []
     for i in range(list_length):
         try:
-            new.append(my_list_1[i] / my_list_2[i])
+            elem = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            new.append(0)
+            elem = 0
         except TypeError:
             print("wrong type")
-            new.append(0)
+            elem = 0
         except IndexError:
             print("out of range")
-            new.append(0)
+            elem = 0
+        finally:
+            new.append(elem)
     return new
