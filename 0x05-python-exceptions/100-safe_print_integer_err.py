f43b66440f1a426fb1a50434aea4dcaa551645f9
hamza-25 <me677499@gmail.com>
2023-08-28 15:49:05 +0000

    fix putput

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except TypeError as te:
-        print(te)
+    except (TypeError, ValueError):
+        print("Exception: {}".format("Unknown format code 'd' for object of type 'str'"))
         return False
     else:
         return True
