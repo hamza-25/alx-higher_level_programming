3b8cba8d6faa88e9e5aacf13ee4658edd9072c4e
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-29 20:23:56 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,12 +7,12 @@ class Square:
     """Represent of square."""
     def __init__(self, size=0, position=(0, 0)):
         """init function ran every time we create an object.
-        
+
          Args:
             size (int): square size.
             position (int, int): The square position.
 
-            
+
         """
         self.__size = size
         self.__position = position
