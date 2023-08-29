3a9f536e1f645e048cae6e1067c176a6ba0e7f0a
hamza-25 <me677499@gmail.com>
2023-08-29 13:41:21 +0000

    add method area that return square of area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""define an empty class Square."""
+
+
+class Square:
+    """Represent of square."""
+    def __init__(self, size=0):
+        """init function ran every time we create an object."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Method that return current square area."""
+        result = self.__size * self.__size
+        return (result)
