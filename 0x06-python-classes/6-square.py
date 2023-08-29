34956fcb96937ca8704c895a6313f2d4b1a3080e
hamza-25 <me677499@gmail.com>
2023-08-29 18:05:40 +0000

    fix some bugs

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,6 +42,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            [print("") for i in range(0, self.__position[1])]
             for i in range(self.__size):
                 if self.__position[1] > 0:
                     print("_" * (self.__position[0]), end="")
