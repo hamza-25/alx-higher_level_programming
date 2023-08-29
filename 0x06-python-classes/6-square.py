3df9ce043861af12842ae7728156fdc54e433980
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-08-29 19:52:15 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,7 +51,9 @@ class Square:
         else:
             [print("") for i in range(0, self.__position[1])]
             for i in range(self.__size):
-                if not (self.__position[1] > 0):
+                if self.__position[1] > 0:
+                    pass
+                else:
                     print(" " * (self.__position[0]), end="")
                 for j in range(self.__size):
                     print("#", end="")
