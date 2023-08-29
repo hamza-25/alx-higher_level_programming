604260f49d60f79b595c51c9a36e8b6834725040
hamza-25 <me677499@gmail.com>
2023-08-29 12:36:05 +0000

    class Square that defines a square with private attrb

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""define an empty class Square."""
+
+
+class Square:
+    """Represent of square."""
+    def __init__(self, size):
+        """init function ran every time we create an object."""
+        self.__size = size
