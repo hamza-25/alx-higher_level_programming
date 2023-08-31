0665ec2abb00eba2daba6c000d326f10a8e08e7c
hamza-25 <me677499@gmail.com>
2023-08-31 15:07:18 +0000

    first comit and task with test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+implmt addition function
+
+"""
+
+
+def add_integer(a, b=98):
+
+    """
+    add two integer and return sum.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    sum = int(a + b)
+    return (sum)
