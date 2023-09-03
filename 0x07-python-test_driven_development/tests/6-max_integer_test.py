fb7c00dbe6be967a9ade038e457ee40b81005200
hamza-25 <me677499@gmail.com>
2023-09-03 21:12:33 +0000

    add test in test folder

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,8 +8,14 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_get_max_value(self):
-        list_num = [100, 45, 52, 101]
-        self.assertEqual(max_integer(list_num), 101)
+        self.assertEqual(max_integer([100, 45, 52, 101]), 101)
+        self.assertEqual(max_integer([5,5,5,5]), 5)
+        self.assertEqual(max_integer([-1,7,4,10]), 10)
+        self.assertEqual(max_integer([-10,-1,-70,-5]), -1)
+        self.assertEqual(max_integer([101, 45, 52, -101]), 101)
+        self.assertEqual(max_integer([54]), 54)
+        self.assertEqual(max_integer(list(range(20))), 19)
+        self.assertEqual(max_integer([101, 52, 4,52, 4, 101, 4]), 101)
     def test_input_empty_list(self):
         self.assertEqual(max_integer([]), None)
 if __name__ == "__main__":
