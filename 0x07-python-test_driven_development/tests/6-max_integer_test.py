969d56ba5a989c5bdcc705d964d3df249f8364ce
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-03 23:57:44 +0100

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,7 +7,13 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """class TestMaxInteger inhert TestCase.
+    """
+
+    
     def test_get_max_value(self):
+        """funcion test_get_max_value that test mult case of list.
+        """
         self.assertEqual(max_integer([100, 45, 52, 101]), 101)
         self.assertEqual(max_integer([5,5,5,5]), 5)
         self.assertEqual(max_integer([-1,7,4,10]), 10)
@@ -17,6 +23,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer(list(range(20))), 19)
         self.assertEqual(max_integer([101, 52, 4,52, 4, 101, 4]), 101)
     def test_input_empty_list(self):
+        """function that test with empty list.
+        """
         self.assertEqual(max_integer([]), None)
 if __name__ == "__main__":
     unittest.main()
