b830887fa234f71f268a7f6dce827a88de8765da
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-04 20:59:26 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle():
     """
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """init function."""
