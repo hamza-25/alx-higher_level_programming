468ecc37444a5bec2df1b5fcb8aed6bd94d05338
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-04 16:36:39 +0100

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle():
     """define rectangle class."""
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """init function."""
