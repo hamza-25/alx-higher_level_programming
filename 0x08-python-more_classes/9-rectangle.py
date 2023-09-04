d2c4c414a33dd97e323ef1383b2819adafbdeebb
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-04 20:35:34 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle():
 
     def __init__(self, width=0, height=0):
         """init function."""
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
         Rectangle.number_of_instances += 1
 
     @property
