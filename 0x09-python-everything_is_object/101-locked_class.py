9a51fab368b16cac732d4455d312ca6b3e02183b
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-05 17:42:22 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,6 +4,7 @@
 
 class LockedClass:
     """representaion LockedClass
+    prevents the user from dynamically creating new instance attributes
+    except if the new instance attribute is called first_name
     """
-
     __solts__ = ["first_name"]
