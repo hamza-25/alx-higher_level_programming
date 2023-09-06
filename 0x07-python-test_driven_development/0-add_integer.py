b42899f997cc2f7c18650b32b2bfa6ed468c1e8b
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-06 11:37:49 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,7 +13,7 @@ def add_integer(a, b=98):
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     sum = int(a + b)
     return (sum)
