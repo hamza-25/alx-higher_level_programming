8253574e3aea11ed1f25527d3ce8a68ab40d909a
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-12 23:40:19 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,7 +15,6 @@ class Student:
     def to_json(self, attrs=None):
         """Get a dictionary representation student.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
