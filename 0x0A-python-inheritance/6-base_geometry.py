a1e629f7cce9f0a42670d70a5c6a7d26332d93a9
hamza-25 <me677499@gmail.com>
2023-09-14 01:05:18 +0000

    basegeo not impl

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines Basegeometry class."""
+
+
+class BaseGeometry:
+    """base geometry representation."""
+
+    def area(self):
+        """raise error not implemented."""
+        raise Exception("area() is not implemented")
