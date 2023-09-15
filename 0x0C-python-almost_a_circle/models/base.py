59e53382578643f870fc61289bb66a25371e84b5
hamza-25 <me677499@gmail.com>
2023-09-15 23:37:20 +0000

    creat base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a base class"""
+
+class Base:
+    """representation of class base"""
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
