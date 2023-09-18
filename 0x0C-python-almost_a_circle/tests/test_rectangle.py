bd5f382cf7d84483de726725d6df69825b7bf346
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-18 21:42:55 +0100

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""define rectangle file testing"""
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class Rectangle_instantiation_test(unittest.TestCase):
+    """testing instantiation of Rectangle class."""
+
+    def test_rectangle_is_base(self):
+        self.assertIsInstance(Rectangle(10, 2), Base)
+
+if __name__ == "__main__":
+    unittest.main()
