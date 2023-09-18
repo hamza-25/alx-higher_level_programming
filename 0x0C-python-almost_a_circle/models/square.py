ab594821745c30959c944360eb7e94acaae033d0
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-18 17:07:04 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,20 +5,23 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
+        """square inintialization"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """func that set size of square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        '''if value <= 0:
+        if value <= 0:
             raise ValueError("size must be > 0")
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")'''
+            raise TypeError("size must be an integer")
         self.__width = value
         self.__height = value
 
     def __str__(self):
+        """func that print info about suqare"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
