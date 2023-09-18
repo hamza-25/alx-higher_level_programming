2ff873ea8b1d1fef8393de8e439d19e0044a32ad
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-18 23:42:09 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    def __init__(self, size, x=0, y=0, id=None):
+    """Representation of square."""
+
+   def __init__(self, size, x=0, y=0, id=None):
         """square inintialization
             Args:
                 size: height of rectangle
