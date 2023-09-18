7b91aca3603deab8109ed161c58290b134378ff5
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-18 21:45:58 +0100

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 """define unittest for base.py"""
 import unittest
-from models.base import Base
-
-class Base_instantiation_test(unittest.TestCase):
-    """testing instantiation of the Base class."""
-
-    def test_no_arg(self):
-        ob1 = Base()
-        ob2 = Base()
-        self.assertEqual(ob1.id, ob2.id - 1)
 
 if __name__ == "__main__":
     unittest.main()
