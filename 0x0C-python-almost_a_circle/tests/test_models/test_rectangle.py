3c3222ec3bbaf43225d9aae6bec81516dc285a16
hamza-25 <me677499@gmail.com>
2023-09-21 17:54:12 +0000

    fix unit test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """define rectangle file testing"""
+from models.rectangle import Rectangle
 
 import unittest
+
+class TestCaseRectangleClass(unittest.TestCase):
+    def print_width_height_rectangle(self):
+        obj1 = Rectangle(10, 5)
+        self.assertEqual(obj1.width, 10)
+        self.assertEqual(obj1.height, 5)
 
 if __name__ == "__main__":
     unittest.main()
