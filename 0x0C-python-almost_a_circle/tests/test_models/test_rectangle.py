58c58a647f323266fb917d22bd2cfeee29d6c44f
hamza-25 <me677499@gmail.com>
2023-09-21 17:56:43 +0000

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,7 +5,7 @@ from models.rectangle import Rectangle
 import unittest
 
 class TestCaseRectangleClass(unittest.TestCase):
-    def print_width_height_rectangle(self):
+    def test_print_width_height_rectangle(self):
         obj1 = Rectangle(10, 5)
         self.assertEqual(obj1.width, 10)
         self.assertEqual(obj1.height, 5)
