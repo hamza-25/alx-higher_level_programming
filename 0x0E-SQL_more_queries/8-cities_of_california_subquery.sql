b92ee51f1324b1fbae63fc437c2d44dd9f849aea
hamza-25 <me677499@gmail.com>
2023-10-18 15:04:23 +0000

    use forign key

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- script that lists all the cities of California
+-- that can be found in the database hbtn_0d_usa 
+SELECT c.id, c.name
+FROM states as s, cities as c 
+WHERE c.state_id = s.id
+ORDER BY c.id ASC;
