cd08a2fa285d65a17c9de97b8594882f74437162
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-10-18 18:35:30 +0100

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,13 +1,11 @@
--- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tv_shows.title
+-- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT title
 FROM tv_shows
-WHERE tv_shows.title NOT IN 
-(SELECT tv_shows.title
+WHERE title NOT IN 
+(SELECT title
 FROM tv_shows
-LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy')
-GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC;
+GROUP BY title
+ORDER BY title ASC;
