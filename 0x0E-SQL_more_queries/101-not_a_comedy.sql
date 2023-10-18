93da3eeb36639c32186a1c33a318f8796ce1d02d
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-10-18 18:31:28 +0100

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,9 +4,9 @@ FROM tv_shows
 WHERE tv_shows.title NOT IN 
 (SELECT tv_shows.title
 FROM tv_shows
-RIGHT JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-RIGHT JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = 'Comedy')
 GROUP BY tv_shows.title
