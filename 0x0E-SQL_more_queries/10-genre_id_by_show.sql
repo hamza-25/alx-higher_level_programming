b67c782d27775f6a7f81d616dfd265913413faff
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-10-18 17:08:02 +0100

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,5 @@ FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id;
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
