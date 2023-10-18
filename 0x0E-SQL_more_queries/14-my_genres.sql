8d45e07a6985bdb950b99a2f44de2748f7b265ad
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-10-18 17:55:04 +0100

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,8 @@ SELECT tv_genres.name
 FROM tv_show_genres
 RIGHT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY tv_genres.name
 ORDER BY tv_genres.name ASC;
