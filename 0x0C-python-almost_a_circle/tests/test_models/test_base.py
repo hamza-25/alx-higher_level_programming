173b29d0bc45bc91e5ed560499d742ed534834ce
hamza-25 <me677499@gmail.com>
2023-11-13 13:33:33 +0000

    fisrt commit

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 """define unittest for base.py"""
 import unittest
+from models.base import Base
+
+class TestBaseClass(unittest.TestCase):
+
+    def test_same_id(self):
+        obj1 = Base()
+        #obj2 = Base(1)
+        self.assertEqual(print(obj1.id), 1)
+        #self.assertEqual(print(obj1.id), 2)
 
 if __name__ == "__main__":
     unittest.main()
