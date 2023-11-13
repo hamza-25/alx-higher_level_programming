7f65a5aff7e1259bafcc68bbcc753a82acc45c29
hamza-25 <me677499@gmail.com>
2023-11-13 15:46:07 +0000

    fix semi standard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -11,8 +11,8 @@ if (argLenght === 2) {
   console.log('Not a number');
 } else if (argLenght >= 3) {
   if (isNaN(argv[2])) {
-	console.log('Not a number');
+    console.log('Not a number');
   } else {
-	  console.log('My number: ' + parseInt(argv[2]));
+    console.log('My number: ' + parseInt(argv[2]));
   }
 }
