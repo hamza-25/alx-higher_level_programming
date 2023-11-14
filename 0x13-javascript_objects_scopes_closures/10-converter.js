969c1c5095d38e959b03b4007976a74e067cb8ba
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-11-14 21:22:03 +0100

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (number) {
-    /*return number.toString(base);*/
-    return parseInt(number, base);
+    return number.toString(base);
   };
 };
