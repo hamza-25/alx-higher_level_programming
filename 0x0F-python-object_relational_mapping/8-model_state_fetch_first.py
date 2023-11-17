41678e2522a5c8bc79bd1046961c8895ed7c7af8
hamza-25 <me677499@gmail.com>
2023-11-17 20:17:22 +0000

    retrirve first column by id

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+if __name__ == '__main__':
+    str_db = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(str_db.format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.bind = engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    all_states = session.query(State).order_by(State.id.asc()).limit(1).all()
+    if all_states:
+        for state in all_states:
+            print("{}: {}".format(state.id, state.name))
+    else:
+        print('Nothing')
+    session.close()
