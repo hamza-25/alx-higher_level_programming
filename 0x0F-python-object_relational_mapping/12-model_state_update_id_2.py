393248693c7179f36217aa52e0fe7633df25041e
hamza-25 <me677499@gmail.com>
2023-11-17 23:00:58 +0000

    update row by id

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+if __name__ == '__main__':
+    str_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(str_db.format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_update = (session.query(State).filter_by(id=2).first())
+    state_update.name = 'New Mexico'
+    session.commit()
+    session.close()
