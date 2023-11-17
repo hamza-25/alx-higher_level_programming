67b8508c533ab6be1e18a98b73dbd24d2528a3e4
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-11-17 21:52:02 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    all_states = session.query(State).order_by(State.id).filter(State.name.like('%a%')).all()
+    all_states = session.query(State).filter(State.name.like('%a%')).all()
     for state in all_states:
         print("{}: {}".format(state.id, state.name))
     session.close()
