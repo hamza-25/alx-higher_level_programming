9d142fb7e9080569acef0336eacb009594cfac49
hamza-25 <me677499@gmail.com>
2023-11-17 14:15:09 +0000

    fix docs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""List all state order by id"""
 import MySQLdb
 from sys import argv
-host = 'localhost'
-db = MySQLdb.connect(host=host, user=argv[1], passwd=argv[2], db=argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for state in rows:
-    print(state)
-db.commit()
+
+if __name__ == '__main__':
+    host = 'localhost'
+    db = MySQLdb.connect(host=host, user=argv[1], passwd=argv[2], db=argv[3])
+    
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
+    db.commit()
