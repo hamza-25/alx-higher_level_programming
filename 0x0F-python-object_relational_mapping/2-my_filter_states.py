47977fbdc260c32d508e58fb84742bd1d0b4b2c9
hamza-25 <me677499@gmail.com>
2023-11-17 15:08:57 +0000

    filter one row

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values by user arg
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+
+    q = f"SELECT * FROM states WHERE name = '%s' \
+    ORDER BY states.id ASC" % argv[4]
+    cur.execute(q)
+
+    state = cur.fetchone()
+    print(state)
+    cur.close()
+    db.close()
