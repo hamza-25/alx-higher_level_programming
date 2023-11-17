50767c29f5f2b594a6a40ae147cb4dad20d1002a
hamza-25 <me677499@gmail.com>
2023-11-17 14:35:15 +0000

    fetch states start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""List all state order by id"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    host = 'localhost'
+    db = MySQLdb.connect(host=host, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    q = "SELECT * FROM states WHERE name LIKE 'N%'ORDER BY states.id ASC"
+    cur.execute(q)
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
+    cur.close()
+    db.close()
