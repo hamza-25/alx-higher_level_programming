81d8050bf3f9279bded65d3f09f3002b97f6f426
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-11-17 21:26:20 +0100

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,8 +20,9 @@ if __name__ == '__main__':
 
     all_states = session.query(State).first()
     if all_states:
-        for state in all_states:
-            print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(all_states.id, all_states.name))
+        #for state in all_states:
+        #    print("{}: {}".format(state.id, state.name))
     else:
         print('Nothing')
     session.close()
