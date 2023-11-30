e51ed8d85c8b4542a4fb212d29a6980bed7c613f
hamza-25 <me677499@gmail.com>
2023-11-30 15:49:57 +0000

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-if [ "$(curl -I -s $1 | grep 200 | cut -d ' ' -f 2)" = "200" ]; then curl -X GET -s $1; fi
+#if [ "$(curl -I -s $1 | grep 200 | cut -d ' ' -f 2)" = "200" ]; then curl -X GET -s $1; fi
+curl -sL $1
