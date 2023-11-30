fd3b9b73437f226a90c1eadfc403b869f3bc5304
hamza-25 <me677499@gmail.com>
2023-11-30 18:19:57 +0000

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,17 +5,20 @@
 def find_peak(list_of_integers):
     """representation of a peak function"""
     length = len(list_of_integers)
+
     if length == 0:
         return None
+
     if length == 1:
         return list_of_integers[0]
 
     if list_of_integers[0] >= list_of_integers[1]:
         return list_of_integers[0]
 
+    if list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
+
     for i in range(1, length - 1):
         if (list_of_integers[i] >= list_of_integers[i - 1]
                 and list_of_integers[i] >= list_of_integers[i + 1]):
             return list_of_integers[i]
-    if list_of_integers[-1] >= list_of_integers[-2]:
-        return list_of_integers[-1]
