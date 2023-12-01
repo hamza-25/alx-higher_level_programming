c4860ef67244457dec53b2e38fdf116f29661799
hamza-25 <me677499@gmail.com>
2023-12-01 19:47:24 +0000

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
     r = requests.get(url)
     if r.status_code == 200:
         r_json = r.json()
+        count = 0
         for commit in r_json:
             print("{}: {}".format(
                 commit['sha'], commit['commit']['author']['name']))
+            if count == 9:
+                break
+            count += 1
