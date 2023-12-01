bae06dbba082615058078e5a87c55d7388a8ce94
hamza-25 <me677499@gmail.com>
2023-12-01 18:29:08 +0000

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,13 +5,14 @@ import requests
 import sys
 if __name__ == "__main__":
     q = ''
-    if sys.argv[1]:
+    if len(sys.argv[1]) > 1:
         q = sys.argv[1]
     payload = {"q": q}
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    if isinstance(r, dict) and len(r) == 0:
+    r_json = r.json()
+    if isinstance(r_json, dict) and len(r_json) == 0:
         print("No result")
-    elif isinstance(r, dict):
-        print("[{}] {}".format(r['id'], r['name']))
+    elif isinstance(r_json, dict):
+        print("[{}] {}".format(r_json['id'], r_json['name']))
     else:
         print("Not a valid JSON")
