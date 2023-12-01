be6e6194d28f373cda7ef806ee201d279acb8f0b
hamza-25 <me677499@gmail.com>
2023-12-01 14:25:12 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" """
+"""define url module"""
 
 
 if __name__ == "__main__":
     import urllib.request as request
-    resp = request.urlopen("https://alx-intranet.hbtn.io/status")
-    print("Body response:\n\t- type: {}\n\t- content: b'{}'".format(type(resp.read()), resp.msg))
-    print("\t- utf8 content: {}".format(resp.msg))
-
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+        print("Body response:\n\t- type: {}\n\t- content: b'{}'".format(
+            type(resp.read()), resp.msg))
+        print("\t- utf8 content: {}".format(resp.msg))
