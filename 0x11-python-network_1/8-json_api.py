4ea685ba9405d3ff24d98c49a4a47db9b98961bd
hamza-25 <me677499@gmail.com>
2023-12-01 18:31:22 +0000

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ import requests
 import sys
 if __name__ == "__main__":
     q = ''
-    if len(sys.argv[1]) > 1:
+    if len(sys.argv) > 1:
         q = sys.argv[1]
     payload = {"q": q}
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
