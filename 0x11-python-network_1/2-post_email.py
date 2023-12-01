c4eaa9718d4fc040e2d88710f6f101c3b5ca4a80
hamza-25 <me677499@gmail.com>
2023-12-01 15:33:54 +0000

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,8 +2,12 @@
 """define url module"""
 
 import urllib.request as request
+import urllib.parse as parse
 import sys
 if __name__ == "__main__":
-    url = sys.argv[1] + '?email=' + sys.argv[2]
-    with request.urlopen(url) as f:
+    email = sys.argv[2]
+    url = sys.argv[1]
+    convert = parse.urlencode(email).encode("ascii")
+    req = request.Request(url, convert)
+    with request.urlopen(req) as f:
         print(f.read().decode('utf-8'))
