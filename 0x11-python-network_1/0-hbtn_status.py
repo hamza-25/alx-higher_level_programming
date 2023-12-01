a99254b8841b2a999fd3afdf09bf200984ed197d
hamza-25 <me677499@gmail.com>
2023-12-01 14:29:32 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,8 @@
 
 
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+    import urllib
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
         print("Body response:\n\t- type: {}\n\t- content: b'{}'".format(
             type(resp.read()), resp.msg))
         print("\t- utf8 content: {}".format(resp.msg))
