dbaaa903bfb67848afd9f328bc2a5226d6fd95f9
hamza-25 <me677499@gmail.com>
2024-01-03 15:31:02 +0100

    update

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+request.get(args[2], (err, response) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('code: ', response.statusCode);
+});
